523cb7b1b7f75dc2dcac27bbb0d2cc40ddd3a87d
VinGitonga <gitongavincent64@gmail.com>
2025-05-10 09:12:29 +0300

    Update API base URL in env.ts and add .dockerignore and Dockerfile for backend setup

--- novix-client/src/env.ts
+++ novix-client/src/env.ts
@@ -1,2 +1,3 @@
-export const API_CUSTOMA_AGENTS_BASE_URL = "http://localhost:9000/api";
+// export const API_CUSTOMA_AGENTS_BASE_URL = "http://localhost:9000/api";
+export const API_CUSTOMA_AGENTS_BASE_URL = "http://tk84k40kogo4kscsgckgww0s.143.198.238.202.sslip.io/api";
 export const API_URL = "http://localhost:6896/api"
